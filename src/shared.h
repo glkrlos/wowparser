f960b76e1ea74642f0614d7321680636e7fd63a7
glkrlos <glkrlos@hotmail.com>
2022-03-01 14:42:22 -0600

    Se implementa soporte basico para aumentar los contadores para los archivos conocidos
    Signed-off-by: glkrlos <glkrlos@hotmail.com>

--- src/shared.h
+++ src/shared.h
@@ -12,6 +12,16 @@ enum enumFieldTypes
     type_BOOL   = 6,
 };
 
+enum enumFileFormat
+{
+    unkFile = 0,
+    dbcFile = 1,
+    adbFile = 2,
+    db2File = 3,
+    wdbFile = 4,
+    csvFile = 5,
+};
+
 // __FUNCSIG__ para imprimir el nombre de la funcion completa y localizar algun fallo si algo no sale bien
 
 class DBFileReader
@@ -22,6 +32,28 @@ class DBFileReader
             static DBFileReader instance;
             return instance;
         }
+        void IncreaseCounters(enumFileFormat FileFormat)
+        {
+            switch (FileFormat)
+            {
+                case dbcFile: _dbcFiles++; break;
+                case adbFile: _adbFiles++; break;
+                case db2File: _db2Files++; break;
+                case wdbFile: _wdbFiles++; break;
+                case csvFile: _csvFiles++; break;
+                case unkFile:
+                default:
+                    _unkFiles++;
+                    break;
+            }
+        }
+    private:
+        unsigned int _dbcFiles = 0;
+        unsigned int _adbFiles = 0;
+        unsigned int _db2Files = 0;
+        unsigned int _wdbFiles = 0;
+        unsigned int _csvFiles = 0;
+        unsigned int _unkFiles = 0;
 };
 
 #define sDBFileReader DBFileReader::Instance()
