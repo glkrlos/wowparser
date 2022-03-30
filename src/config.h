436686608b7d9d8ec6728b54cfb8b9f9978d72cf
glkrlos <glkrlos@hotmail.com>
2022-03-30 00:49:11 -0600

    Rebuild the whole structure of modules and files, and using only english commit descriptions
    Signed-off-by: glkrlos <glkrlos@hotmail.com>

--- src/config.h
+++ src/config.h
@@ -3,8 +3,6 @@
 
 #include "pch.h"
 #include "shared.h"
-#include "tinyxml2.h"
-#include "log.h"
 
 #ifdef _WIN32
     #include "win32/dirent.h"
@@ -12,23 +10,11 @@
     #include <dirent.h>
 #endif
 
-using namespace tinyxml2;
-
 class Config
 {
     public:
-        Config();
-        bool LoadConfiguarionFile();
         void AddFilesToList(string, string, string, bool, string);
-        unsigned int GetFormatedRecordSize(string);
-        vector<enumFieldTypes> GetFormatedFieldTypes(string);
-        unsigned int GetFormatedTotalFields(string structure)
-        {
-            return structure.empty() ? 0 : structure.size();
-        }
-        map<string, string> fileNames;
     private:
-        bool IsValidFormat(string);
         void InsertIfFileNotExist(string file, string structure)
         {
             map<string, string>::iterator Found = fileNames.find(file);
@@ -38,6 +24,6 @@ class Config
             fileNames.insert(pair<string, string>(file, structure));
         }
     protected:
-        XMLDocument XMLdoc;
+		map<string, string> fileNames;
 };
 #endif
