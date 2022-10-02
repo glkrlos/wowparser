8e97b4e3800167781b175d048042ff6b8a8d5509
glkrlos <glkrlos@hotmail.com>
2022-10-01 19:32:53 -0600

    Funcion agregada para determinar si es un archivo ASCII o Binario
    Signed-off-by: glkrlos <glkrlos@hotmail.com>

--- src/module_parser.h
+++ src/module_parser.h
@@ -31,6 +31,22 @@ class module_parser
         }
         bool Load();
     private:
+        bool FileIsASCII()
+        {
+            int c;
+            ifstream a(_fileName);
+            while ((c = a.get()) != EOF && c <= 127)
+                ;
+
+            if (a.is_open())
+                a.close();
+
+            if (c == EOF)
+                return true;
+
+            return false;
+
+        }
         bool CheckStructure();
         bool PredictFieldTypes();
 
