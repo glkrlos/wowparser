16a6e77a9082c9ab788811ff9d1d3e0b513d5d09
glkrlos <glkrlos@hotmail.com>
2022-11-11 17:18:20 -0600

    eliminando archivo que no se usa mas
    Signed-off-by: glkrlos <glkrlos@hotmail.com>

--- src/module_csv_reader.h
+++ src/module_csv_reader.h
@@ -3,7 +3,22 @@
 
 #include "pch.h"
 #include "shared.h"
-#include "module_dbc_writer.h"
+
+struct structRecord
+{
+    enumFieldTypes fieldType;
+    string fieldValue;
+};
+
+struct structFileInfo
+{
+    const char* fileName;
+    unsigned int recordSize;
+    unsigned int totalRecords;
+    unsigned int totalFields;
+    unsigned int stringSize;
+    string stringTexts;
+};
 
 class SaveFileInfo
 {
@@ -64,7 +79,6 @@ class CSV_Reader : public SaveFileInfo
         CSV_Reader(const char *);
         ~CSV_Reader();
         bool LoadCSVFile();
-        FileData *ExportFileData() { return new FileData(_structFileInfo, fileData); }; // no usada aun
     private:
         bool ExtractFields(string, map<unsigned int, string> &);
         map<unsigned int, string> GetFields(string);
