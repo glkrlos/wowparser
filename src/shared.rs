7824cf97efe434030ca54b91420252c2d0303a34
glkrlos <glkrlos@hotmail.com>
2023-11-27 17:19:53 -0600

    Removed log.h and log.cpp, now is converted to rust
    Signed-off-by: glkrlos <glkrlos@hotmail.com>

--- src/shared.rs
+++ src/shared.rs
@@ -1,17 +1,18 @@
 use lazy_static::lazy_static;
+use std::sync::{Mutex, Arc};
 
 pub struct Sshared;
 
 impl Sshared {
-    pub fn instance() -> &'static Sshared {
-        static INSTANCE: Sshared = Sshared;
-        &INSTANCE
-    }
     pub fn show(&self) {
         println!("Shared");
     }
 }
 
 lazy_static! {
-    static ref SHARED: &'static Sshared = Sshared::instance();
+    static ref INSTANCE: Arc<Mutex<Sshared>> = Arc::new(Mutex::new(Sshared));
+}
+
+pub fn instance() -> std::sync::MutexGuard<'static, Sshared> {
+    INSTANCE.lock().unwrap()
 }
