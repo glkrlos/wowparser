c0164776aa7725944f5f6ae3d6bff1fcf4f49eb7
glkrlos <glkrlos@hotmail.com>
2024-04-08 18:38:01 -0600

    cmake: Added cmake project
    Signed-off-by: glkrlos <glkrlos@hotmail.com>

--- revision.sh
+++ revision.sh
@@ -3,12 +3,13 @@ REV_HASH=`git describe --match init --dirty=+ --abbrev=12`
 set -- "$REV_HASH"
 IFS='-'; declare -a Array=($*)
 REVISION=${Array[1]}
-HASH=${Array[2]:1} 
+HASH=${Array[2]:1}
 DATE=`git show -s --format=%ci`
+OUTPATH="$(dirname "$(readlink -f "$0")")"
 
-echo "#ifndef REVISION_H" > revision.h
-echo "#define REVISION_H" >> revision.h
-echo "    #define REVISION \"$REVISION\"" >> revision.h
-echo "    #define HASH \"$HASH\"" >> revision.h
-echo "    #define DATE \"$DATE\"" >> revision.h
-echo "#endif // REVISION_H" >> revision.h
+echo "#ifndef REVISION_H" > $OUTPATH/revision.h
+echo "#define REVISION_H" >> $OUTPATH/revision.h
+echo "    #define REVISION \"$REVISION\"" >> $OUTPATH/revision.h
+echo "    #define HASH \"$HASH\"" >> $OUTPATH/revision.h
+echo "    #define DATE \"$DATE\"" >> $OUTPATH/revision.h
+echo "#endif // REVISION_H" >> $OUTPATH/revision.h
