1ce648f7a6064065dd738784e81924492a5e9db5
glkrlos <glkrlos@hotmail.com>
2024-05-04 16:22:12 -0600

    Moved some constants to version.h
    Signed-off-by: glkrlos <glkrlos@hotmail.com>

--- src/pch.h
+++ src/pch.h
@@ -11,33 +11,9 @@
 #include <memory>
 #include <utility>
 
-#define CODENAME "Huamantla"
-#define VERSION "4.0"
-#define CONFIG_FILENAME "wowparser4.xml"
-
-#ifdef __linux__
-    #include "platform/linux/getch.h"
-    #define OS "Linux"
-
-    #ifdef __x86_64__
-        #define ARQUITECTURA "AMD64"
-    #else
-        #define ARQUITECTURA "i386"
-    #endif
-#elif defined _WIN32 || defined _WIN64
-    //#pragma warning(disable: 4996 4267)
-    #include <conio.h>
-    #define OS "Windows"
-
-    #ifdef _WIN64
-        #define ARQUITECTURA "x64"
-    #else
-        #define ARQUITECTURA "x86"
-    #endif
-#endif
-
 using namespace std;
 
 #include "log.h"
+#include "version.h"
 
 #endif
